306aed52131f6627daeec93727bac86e84dc14b9
Yonoveleta <thetruevayne@gmail.com>
2024-11-15 21:13:17 +0100

    Added more functions

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import numpy as np
 import random
 
+H_4_7 = np.array([
+    [1, 0, 0, 0, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1],
+    [0, 0, 1, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1],
+], dtype=np.uint8)
+
+G_12_23 = np.array([
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+], dtype=np.uint8)
+
 def get_charcodes(filename):
     charcodes = {}
 
@@ -11,7 +33,7 @@ def get_charcodes(filename):
     return charcodes
 
 
-def letters_to_bits(msg, charcodes):
+def chars_to_bits(msg, charcodes):
     code = ""
     for letter in msg:
         code += charcodes[letter]
@@ -25,13 +47,28 @@ def apply_noise(bits, probability):
         
     return bits_after_noise
 
-   
+def bits_to_array(bits):
+    return np.array([np.uint8(bit) for bit in bits], dtype=np.uint8)
+
+def bits_to_chars(bits, charcodes):
+    bit_size = len(charcodes[next(iter(charcodes))])
+    splitted_bits = [bits[bit:bit+bit_size] for bit in range(0, len(bits), bit_size)]
+
+    charcodes_chars = list(charcodes.keys())
+    charcodes_bits = list(charcodes.values())
+
+    return ''.join([charcodes_chars[charcodes_bits.index(bits)] for bits in splitted_bits])
+
 charcodes = get_charcodes('files/charactcodif.txt')
-coded = letters_to_bits("cada", charcodes)
-print(coded)
 
-print(apply_noise(coded, 0.04))
+bits = chars_to_bits("cada", charcodes)
+print(bits)
 
+bits_after_noise = apply_noise(bits, 0.04)
+print(bits_after_noise)
 
+chars = bits_to_chars(bits, charcodes)
+print(chars)
 
-
+bits_after_noise_chars = bits_to_chars(bits_after_noise, charcodes)
+print(bits_after_noise_chars)
