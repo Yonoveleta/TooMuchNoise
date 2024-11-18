1ff624100eb77450d3772cf362639f68b7a2f55a
Yonoveleta <thetruevayne@gmail.com>
2024-11-18 18:33:08 +0100

    added redundancy

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def get_charcodes(filename):
             
     return charcodes
 
-
 def chars_to_bits(msg, charcodes):
     code = ""
     for letter in msg:
@@ -40,10 +39,10 @@ def chars_to_bits(msg, charcodes):
     return code
 
 def apply_noise(bits, probability):
-    flip = {'0': '1', '1' : '0'}
-    bits_after_noise = ''
+    flip = {0:1, 1:0}
+    bits_after_noise = np.array([], dtype=np.uint8)
     for bit in bits:
-        bits_after_noise += flip[bit] if random.random() < probability else bit
+        bits_after_noise = np.append(bits_after_noise, flip[bit] if random.random() < probability else bit)
         
     return bits_after_noise
 
@@ -59,16 +58,23 @@ def bits_to_chars(bits, charcodes):
 
     return ''.join([charcodes_chars[charcodes_bits.index(bits)] for bits in splitted_bits])
 
+def add_redundancy(bits, redundancy_matrix):
+    segment_size = len(redundancy_matrix)
+    segmented_bits = [bits[bit:bit+segment_size] for bit in range(0, len(bits), segment_size)]
+    bits_with_redundancy = np.array([], dtype=np.uint8)
+    for segment in segmented_bits:
+        splitted_segment = bits_to_array(segment)
+        bits_with_redundancy = np.concatenate((bits_with_redundancy, np.matmul(splitted_segment, redundancy_matrix) % 2))
+    
+    return bits_with_redundancy
+        
 charcodes = get_charcodes('files/charactcodif.txt')
 
 bits = chars_to_bits("cada", charcodes)
 print(bits)
 
-bits_after_noise = apply_noise(bits, 0.04)
+bits_with_redundancy = add_redundancy(bits, H_4_7)
+print(bits_with_redundancy)
+
+bits_after_noise = apply_noise(bits_with_redundancy, 0.04)
 print(bits_after_noise)
-
-chars = bits_to_chars(bits, charcodes)
-print(chars)
-
-bits_after_noise_chars = bits_to_chars(bits_after_noise, charcodes)
-print(bits_after_noise_chars)
